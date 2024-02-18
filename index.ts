1a1acbb937f3326feca3a08a141b166992978c1b
Temirlan <temadjumanov@gmail.com>
2024-02-18 16:08:26 +0600

    created post requests for user

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import config from "./config";
 import artistsRouter from "./routers/artists";
 import albumsRouter from "./routers/albums";
 import tracksRouter from "./routers/tracks";
+import userRouter from "./routers/users";
 
 const app = express();
 const port = 8000;
@@ -15,8 +16,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/artists", artistsRouter);
-app.use('/albums', albumsRouter);
-app.use('/tracks', tracksRouter);
+app.use("/albums", albumsRouter);
+app.use("/tracks", tracksRouter);
+app.use("/users", userRouter);
 
 const run = async () => {
   await mongoose.connect(config.mongoose.db);
