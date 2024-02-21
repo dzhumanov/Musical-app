2450667dd0159c40e5e324bedd51d480cb2b691a
Temirlan <temadjumanov@gmail.com>
2024-02-21 15:06:35 +0600

    Created first template of frontend

---
+++ frontend/src/features/artists/Artists.tsx
@@ -0,0 +1,14 @@
+import { Grid, Typography } from "@mui/material";
+
+const Artists = () => {
+  return (
+    <>
+      <Typography variant="h3">Artists</Typography>
+      <Grid item container spacing={2}>
+        {/* Tut budet massiv map */}
+      </Grid>
+    </>
+  );
+};
+
+export default Artists;
